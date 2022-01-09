b89efd9ece884d8f613b66c8e0dff30c459fcd80
UsedAIex <94004933+UsedAIex@users.noreply.github.com>
2022-01-10 00:27:37 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,69 +1,53 @@
 import pygame
-from random import randint
+
 
 class Otobraz:
     def __init__(self):
         pygame.init()
-        size = 300, 300
+        size = width, height = 800, 700
         screen = pygame.display.set_mode(size)
         pygame.display.set_caption("Тесты")
-        rects = []
+
         running = True
-        pygame.draw.rect(screen, (0, 255, 0), (randint(1, 200), randint(1, 200), 100, 100), 0)
+        # Start screen
+        self.draw_to_level_1(screen, width, height)
         while running:
-            a = self.draw(screen)
-            if a == False:
-                break
+            pygame.display.flip()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    x = self.start_game_btn_coords[0]
+                    y = self.start_game_btn_coords[1]
+                    x1 = x + self.start_game_btn_coords[2]
+                    y1 = y + self.start_game_btn_coords[3]
+                    if x < event.pos[0] < x1 and y < event.pos[1] < y1:
+                        self.start(screen)
+
+
+
+        # First level screen
+        # обновляешь экран, формируешь новую картинку, новый игровой цикл,
         pygame.quit()
 
-    def draw(self, screen):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return False
-            if event.type == pygame.MOUSEMOTION:
-                # работает костыльно (если изменится цвет или будет изображение, оно не будет работать)
-                if screen.get_at(event.pos) == pygame.Color(0, 255, 0):
-                    llist = []
-                    screen2 = screen
-                    x, y = event.pos
-                    minus_x = x - 1
-                    minus_y = y - 1
-                    plus_x = x + 1
-                    plus_y = y + 1
-                    for i in range(100):
-                        if screen.get_at((minus_x, y)) == pygame.Color(0, 255, 0):
-                            minus_x = x - i
-                        if screen.get_at((plus_x, y)) == pygame.Color(0, 255, 0):
-                            plus_x = x + i
-                        if screen.get_at((x, plus_y)) == pygame.Color(0, 255, 0):
-                            plus_y = y + i
-                        if screen.get_at((x, minus_y)) == pygame.Color(0, 255, 0):
-                            minus_y = y - i
-                    minus_x -= 1
-                    minus_y -= 1
-                    llist.append(((minus_x, minus_y), (plus_x, minus_y)))
-                    llist.append(((minus_x, minus_y), (minus_x, plus_y)))
-                    llist.append(((plus_x, minus_y), (plus_x, plus_y)))
-                    llist.append(((minus_x, plus_y), (plus_x, plus_y)))
-                    self.draw_line(screen, llist)
-                    print(minus_x, minus_y)
-                    print(plus_x, minus_y)
-                    print(minus_x, plus_y)
-                    print(plus_x, plus_y)
-                    llist.pop()
-                    llist.pop()
-                    llist.pop()
-                    llist.pop()
-            pygame.display.flip()
+    def start(self, screen):
+        screen.fill((0, 0, 0))
 
-    def draw_line(self, screen, lst):
-        for i, j in lst:
-            pygame.draw.line(screen, (255, 0, 0), i, j, width=2)
-            pygame.draw.line(screen, (255, 0, 0), i, j, width=2)
-            pygame.draw.line(screen, (255, 0, 0), i, j, width=2)
-            pygame.draw.line(screen, (255, 0, 0), i, j, width=2)
+    def draw_to_level_1(self, screen, width, height):
+        font = pygame.font.Font(None, 50)
+        text = font.render("Танчики", True, (100, 255, 100))
+        text_start = font.render("Играть", True, (100, 255, 100))
+        text_x = width // 2 - text.get_width() // 2
+        text_x_start = width // 2 - text_start.get_width() // 2
+        text_y = height - 105 - text_start.get_height() // 2
+        text_w = text_start.get_width()
+        text_h = text_start.get_height()
+        screen.blit(text, (text_x, 100))
+        screen.blit(text_start, (text_x_start, screen.get_height() - 120))
+        self.start_game_btn_coords = (text_x_start - 10, text_y - 10,
+                                      text_w + 20, text_h + 20)
+        pygame.draw.rect(screen, (0, 255, 0), self.start_game_btn_coords, 1)
 
 
 if __name__ == '__main__':
     Otobraz()
-
