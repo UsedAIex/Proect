05cda81015b9730a70882952697c06e167d078a7
UsedAIex <94004933+UsedAIex@users.noreply.github.com>
2022-01-10 00:29:08 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import pygame
 
-
+choose_map = None
+map_1 = None
+map_2 = None
 class Otobraz:
     def __init__(self):
         pygame.init()
         size = width, height = 800, 700
         screen = pygame.display.set_mode(size)
         pygame.display.set_caption("Тесты")
+        self.choose_map = choose_map
+        self.map_1 = map_1
+        self.map_2 = map_2
 
         running = True
         # Start screen
-        self.draw_to_level_1(screen, width, height)
+        self.draw_menu(screen, width, height)
         while running:
-            pygame.display.flip()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
@@ -21,8 +25,25 @@ class Otobraz:
                     y = self.start_game_btn_coords[1]
                     x1 = x + self.start_game_btn_coords[2]
                     y1 = y + self.start_game_btn_coords[3]
+                    if self.map_1:
+                        x_map_1 = self.map_1[0]
+                        y_map_1 = self.map_1[1]
+                        x1_map_1 = x_map_1 + self.map_1[2]
+                        y1_map_1 = y_map_1 + self.map_1[3]
+                        if x_map_1 < event.pos[0] < x1_map_1 and y_map_1 < event.pos[1] < y1_map_1:
+                            self.choose_map = 'map_1'
+                            draw_lvl(screen, self.choose_map, self.map_1, self.map_2)
+                    if self.map_2:
+                        x_map_2 = self.map_2[0]
+                        y_map_2 = self.map_2[1]
+                        x2_map_2 = x_map_2 + self.map_2[2]
+                        y2_map_2 = y_map_2 + self.map_2[3]
+                        if x_map_2 < event.pos[0] < x2_map_2 and y_map_2 < event.pos[1] < y2_map_2:
+                            self.choose_map = 'map_2'
+                            draw_lvl(screen, self.choose_map, self.map_1, self.map_2)
                     if x < event.pos[0] < x1 and y < event.pos[1] < y1:
                         self.start(screen)
+            pygame.display.flip()
 
 
 
@@ -33,7 +54,7 @@ class Otobraz:
     def start(self, screen):
         screen.fill((0, 0, 0))
 
-    def draw_to_level_1(self, screen, width, height):
+    def draw_menu(self, screen, width, height):
         font = pygame.font.Font(None, 50)
         text = font.render("Танчики", True, (100, 255, 100))
         text_start = font.render("Играть", True, (100, 255, 100))
@@ -47,6 +68,21 @@ class Otobraz:
         self.start_game_btn_coords = (text_x_start - 10, text_y - 10,
                                       text_w + 20, text_h + 20)
         pygame.draw.rect(screen, (0, 255, 0), self.start_game_btn_coords, 1)
+        self.map_1 = (width - 730, height - 500, 300, 300)
+        pygame.draw.rect(screen, (0, 255, 0), self.map_1, 1)
+        self.map_2 = (width - 350, height - 500, 300, 300)
+        pygame.draw.rect(screen, (0, 255, 0), self.map_2, 1)
+
+
+def draw_lvl(screen, choose_map, map_1, map_2):
+    print(map_1, map_2)
+    if choose_map == 'map_1':
+        pygame.draw.rect(screen, (0, 255, 0), map_1, 0)
+        pygame.draw.rect(screen, (0, 255, 0), map_2, 1)
+    if choose_map == 'map_2':
+        pygame.draw.rect(screen, (0, 255, 0), map_1, 1)
+        pygame.draw.rect(screen, (0, 255, 0), map_2, 0)
+    pygame.display.flip()
 
 
 if __name__ == '__main__':
